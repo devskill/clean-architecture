89e6da969015e21f14fec9272711f8f733ea9190
Jalal Uddin <jalaluddin@devskill.com>
2022-12-18 15:41:22 +0600

    Added primary projects

---
+++ src/Application/Services/CourseService.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        public (IList<Course> courses, int total, int totalAfterFilter) GetFilteredCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Domain/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Course : AggregateRoot<Guid>
+    {
+        public string Title { get; set; }
+        public int Fees { get; set; }
+        public DateTime ClassStartDateTime { get; set; }
+
+    }
+}
